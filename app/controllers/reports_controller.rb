542ecf15d52bddb73dfc80cb7b1c4c30f4d0f890
samrajabeen <samrajabeen.so@gmail.com>
2020-10-31 18:33:07 +0500

    Added Show

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,16 +4,26 @@ class ReportsController < ApplicationController
 
 	end 
 
+
 	def create
 		#render plain :params[:report].inspect
 		@report=Report.new(report_params)
-		@report.save
-
-
-
+		if @report.save
+			flash[:notice]="Report Created Successfully"
+			redirect_to report_path(@report)
+	    else
+	    	render 'new'
+	    end 
 	end 
+
+	def show
+		@report=Report.find(params[:id])
+	end
+
+
 	private
 	   def report_params
 	   	params.require(:report).permit(:title, :description)
 	   end
 end 
+
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,5 @@
 class Report < ActiveRecord::Base
+	validates :title, presence: true, length: {minimum:3, maximum:37}
+	validates :description, presence: true, length: {minimum:3, maximum:37}
+
 end 
